929bd4136805e06325ba34dba16e207d69c7ae77
lricdiaz <62919268+lricdiaz@users.noreply.github.com>
2022-08-16 13:46:32 -0600

    Update Analizador Lexico.cpp

--- Analizador Lexico.cpp
+++ Analizador Lexico.cpp
@@ -5,6 +5,18 @@
 
 using namespace std;
 
+void imprimir();
+void extra();
+void AnalizadorDeCaracter(char letra);
+void Estados();
+void Reservados();
+
+const char *reservadas[] = {"PRINCIPAL", "MIENTRAS", "PARA", "SI", "SINO", "IMPRIMIR", "ALMACENAR", "ENT",
+			    "DEC", "CAD", "V_F", "VERDADERO", "FALSO", "SISINO"};
+int IndicadorTam = sizeof(reservadas)/sizeof(char *);
+int i;
+char palabraIntegrada[50];
+char temp[2];
 int main(){
 	
 }
